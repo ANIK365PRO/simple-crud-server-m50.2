945b06f1996237ece09afa178b302065daa907a5
Anik Mohanta <mohonto05@gmail.com>
2026-05-15 18:45:28 +0600

    added get single id to mongodb.doc for client user details

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
 
@@ -42,7 +42,15 @@ async function run() {
     })
 
 
-
+    app.get('/users/:id', async(req, res) =>{
+      const id = req.params.id
+      const query = {
+        _id: new ObjectId(id)
+      }
+      const user = await usersCollection.findOne(query)
+      console.log('user id ', id, user)
+      res.send(user)
+    })
 
 
 
